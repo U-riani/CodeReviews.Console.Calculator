d199e02fbe770926b876daf840574db389f80fc4
U-riani <sandropapiashvili@gmail.com>
2025-08-04 13:56:07 +0400

    fixed while loop to use calculator more than 1 time

--- CalculatorApp/CalculatorApp/Program.cs
+++ CalculatorApp/CalculatorApp/Program.cs
@@ -68,14 +68,15 @@ namespace CalculatorProgram {
 
                 // Wait for the user to respond before closing.
                 Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
-                if (Console.ReadLine() == "n")
+                if (Console.ReadLine() == "n") {
                     endApp = true;
 
+                    // Dispose of the calculator to close the log file.
+                   
+                    calculator.Finish();
+                }
                 Console.WriteLine("\n"); // Friendly linespacing.
 
-                // Dispose of the calculator to close the log file.
-                calculator.Finish();
-                return;
             }
         }
     }
